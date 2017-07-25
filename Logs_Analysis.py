37e1389c04c936c0a544d6fc23feb0451d7d1fdb
Jason Hester <JMH@Jasons-Air.attlocal.net>
2017-07-24 22:57:53 -0500

    Add style and python main convention as well as for loop to code

--- Logs_Analysis.py
+++ Logs_Analysis.py
@@ -4,18 +4,16 @@ import psycopg2
 
 DBNAME = "news"
 
-#try:
-  #conn = psycopg2.connect(database=DBNAME)
-  #print("I am able to connect to the database")
-#except:
-    #print("I am unable to connect to the database")
-
 def get_popular():
   #"""Return 3 most popular articles from the 'database', based on views."""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
   c.execute("select title, count(*) as views from articles, log where log.path like concat('%',articles.slug,'%') group by title order by views desc limit 3; ")
-  return c.fetchall()
+  row = c.fetchall()
+  print("\n3 Most Popular Articles")
+  for i in row:
+    print("\nArticle: {:} \nViews: {:}" .format(i[0], i[1]))
   db.close
 
-print (get_popular())
+if __name__ == "__main__":
+    get_popular()
