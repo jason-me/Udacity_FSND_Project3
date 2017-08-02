83d5606c2cb947eb858b2496b3b61826ca3318d9
Jason Hester <JMH@Jasons-Air.attlocal.net>
2017-08-01 20:27:47 -0500

    Add query 2 select statement to code

--- Logs_Analysis.py
+++ Logs_Analysis.py
@@ -4,7 +4,7 @@ import psycopg2
 
 DBNAME = "news"
 
-def get_popular():
+def get_pop_art():
   #"""Return 3 most popular articles from the 'database', based on views."""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
@@ -15,5 +15,17 @@ def get_popular():
     print("\nArticle: {:} \nViews: {:}" .format(i[0], i[1]))
   db.close
 
+def get_pop_auth():
+  #"""Return 3 most popular articles from the 'database', based on views."""
+  db = psycopg2.connect(database=DBNAME)
+  c = db.cursor()
+  c.execute("select authors.name, count(*) as views from authors, articles, log where authors.id = articles.author and log.path like concat('%',articles.slug,'%') group by authors.name order by views desc;")
+  row = c.fetchall()
+  print("\n3 Most Popular Articles")
+  for i in row:
+    print("\nArticle: {:} \nViews: {:}" .format(i[0], i[1]))
+  db.close
+
 if __name__ == "__main__":
-    get_popular()
+    get_pop_art()
+    get_pop_auth()
